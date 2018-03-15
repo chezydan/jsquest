88b23e015b23e09306719a395ca00405869685d4
dan <chezydan@gmail.com>
2018-03-15 22:29:26 +0200

    checkbox basic functionality

--- quest.js
+++ quest.js
@@ -8,32 +8,30 @@ document.querySelector('#one').onclick= function(){
 }
 
 
-
+var qindex;
 var questions=[];
 class Question{
-    constructor(question,options,right){
+    constructor(question,options,right, tags){
        this.question= question;
        this.options=options;
        this.right=right; 
+       this.tags=tags;
        questions.push(this);
+
     }
     show(){
-        document.querySelector('#display').innerHTML= "<form id='formy' onsubmit= 'qman.evaluate();' > ";
+        document.querySelector('#display').innerHTML= "<fieldset id='fs'> </fieldset>";
          
-        document.querySelector('#formy').innerHTML += "<br><strong>"+this.question +"</strong><br>";
+        document.querySelector('#fs').innerHTML += "<br><strong>"+this.question +"</strong><br>";
         var i=0;
        this.options.forEach(element => {
-            document.querySelector('#formy').innerHTML += "<input type= checkbox name ="+"q" + " value= " + i + ">"
+            document.querySelector('#fs').innerHTML += "<input type= 'checkbox' name ="+"q"+" class ='qc' "+ " value= " + i + ">"
             + element +"<br>";
             i++;         ;            
         });
-        document.querySelector('#formy').innerHTML += "<button > submit </button>"
+        document.querySelector('#fs').innerHTML += "<button onclick= 'qman.evaluate()'> submit </button>"
     }
-    evaluate(){
-        var answerArr = Array[this.questions.length]
-        alert(anserArr.length);
-        return true;
-    }
+
 
 
 }
@@ -45,26 +43,51 @@ class Qmanager {
 
     display(){
 
-        var i = parseInt(document.querySelector('#number').innerHTML);  
-        if (i< this.questions.length){
-            this.questions[i].show();
+        qindex = parseInt(document.querySelector('#number').innerHTML);  
+        if (qindex< this.questions.length){
+            this.questions[qindex].show();
 
-            document.querySelector('#number').innerHTML= i +1;
+            document.querySelector('#number').innerHTML= qindex +1;
         }//if
     }//display
     evaluate(){
-        var answerArr = new Array (this.questions.length);
-        alert(answerArr .length);
+
+        var answerArr = new Array ;
+        var optionsGiven =  (document.getElementsByClassName('qc'));
+        for (var i = 0 ; i < optionsGiven.length; i++){
+            if (optionsGiven[i].checked)
+                answerArr.push(1);
+            else 
+                answerArr.push(0);
+        console.log(answerArr[i]);
+        }
+                                 
+        
+        
+        document.querySelector('#resPanel').innerHTML=(answerArr+ "  " + questions[qindex].right);
+        document.querySelector('#resPanel').innerHTML+=this.compareArr(answerArr, questions[qindex].right);
+    }
+
+    compareArr(ans, quest){
+        var response = true;
+        for ( var i = 0 ; i < ans.length; i ++)
+            {
+                if (ans[i]!= quest[i]           ){
+                    response = false;
+                    break;
+                }
+            }
+            return response;
+
     }
     }//Qmanager
 
 
-q1 = new Question('asdf?',['asdf', 'two'],[1,2]);
-q2 = new Question("5 * 10",["2","4","10"], [3]);
-q3 = new Question('Burkina Fasso', ['Kampala','Ouagadougou','Lusaka'],[1]);
+q1 = new Question('asdf?',['asdf', 'two'],[0,1]);
+q2 = new Question("5 * 10",["2","4","20"], [0,0,1]);
+q3 = new Question('Burkina Fasso', ['Kampala','Ouagadougou','Lusaka'],[0,1,0], ['geo']);
 q4 = new Question ("למי יש 3? ",["רבוע","משולש"], [2] );
-console.log(questions.length);
-console.log(document.querySelector('#number').innerHTML)
+
 
 qman= new Qmanager(questions);
 
